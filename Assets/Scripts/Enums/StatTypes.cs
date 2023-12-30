900088c19be7e3a3ea2587ac38e46d8b9046c8e2
Taikwy <taikwy.twa@gmail.com>
2023-12-30 04:43:35 -0800

    i lived

--- Assets/Scripts/Enums/StatTypes.cs
+++ Assets/Scripts/Enums/StatTypes.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum StatTypes
-{
-    LV,
-    XP,
-    HP,
-    MHP,
+{   
+    LV,                 //Level - indicates unit's current level
+    XP,                 //Experience - indicates unit's current experience
+    HP,                 //Health Points - indicates unit's current health
+    MHP,                //Max Health Points - indicates unit's max health
+    EP,                 //Energy Points - indicates unit's current energy
+    MEP,                //Max Energy Points - indicates unit's max energy
+    
     EN,
     MEN,
-    AT,
-    DF,
+    AT,                 //Attack
+    DF,                 //Defense
     SK,
     LU,
     AC,
-    SP,
-    MV,
-    AV,
+    SP,                 //Speed
+    MV,                 //Move - indicates unit's max move range
+    AV,                 //Action Value - indicates unit's current action value
     TurnCounter,
     Count
 }
