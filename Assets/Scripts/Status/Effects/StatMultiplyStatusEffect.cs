ef9646d8c993ea1625bf6c4cc72c775cf39c5d9d
Taikwy <taikwy.twa@gmail.com>
2024-02-14 13:24:45 -0800

    updating status effect effects

--- Assets/Scripts/Status/Effects/StatMultiplyStatusEffect.cs
+++ Assets/Scripts/Status/Effects/StatMultiplyStatusEffect.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatMultiplyStatusEffect : MonoBehaviour
+public class StatMultiplyStatusEffect : StatusEffect
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Unit owner;
+	Stats myStats;
+    public int modifyAmount;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void OnEnable (){
+		if (myStats)
+			this.AddObserver( OnCounterWillChange, Stats.WillChangeEvent(StatTypes.AV), myStats );
+	}
+
+	void OnDisable (){
+		this.RemoveObserver( OnCounterWillChange, Stats.WillChangeEvent(StatTypes.AV), myStats );
+	}
+
+	void OnCounterWillChange (object sender, object args){
+		ValueChangeException exc = args as ValueChangeException;
+		MultDeltaModifier m = new MultDeltaModifier(0, 2);
+		exc.AddModifier(m);
+	}
 }
