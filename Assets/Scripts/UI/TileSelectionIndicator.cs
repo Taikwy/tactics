8c477393ccc34c07266587360a52bcb50397be3b
Taikwy <taikwy.twa@gmail.com>
2024-03-05 21:23:27 -0800

    add tile select and target anims

---
+++ Assets/Scripts/UI/TileSelectionIndicator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSelectionIndicator : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    public Animation selectAnim;
+    public Animation targetAnim;
+}
